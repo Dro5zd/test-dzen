a3e24435f86b47cb141a2c66aa8281d8859b10f4
Dro5zd <adrobotyuk@gmail.com>
2023-07-30 14:49:08 +0500

    new site address

--- src/components/SessionCounter.tsx
+++ src/components/SessionCounter.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io('https://inventory-dzencode.netlify.app');
 
 const SessionCounter = () => {
     const [sessionCount, setSessionCount] = useState(0);
